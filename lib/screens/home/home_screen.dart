f969acf93e111b74ea6d59f69bd0d9ae93205964
Adarsh <adarshlp98@gmail.com>
2020-06-24 00:18:29 +0530

    Added video call Functionality

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:elderlycompanion/screens/video_call/video_call.dart';
 import 'package:elderlycompanion/widgets/app_default.dart';
 import 'package:flutter/material.dart';
 
@@ -14,7 +15,15 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: ElderlyAppBar(),
       body: SafeArea(
         child: Column(
-          children: <Widget>[Text('Hello')],
+          children: <Widget>[
+            GestureDetector(
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (_) {
+                    return VideoCall();
+                  }));
+                },
+                child: Text('Hello'))
+          ],
         ),
       ),
     );
